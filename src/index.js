9ed048ccae9214b57beccc5c598200c35c91c6da
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 21:19:48 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Stardrone from "./components/Stardrone";
-import Space from "./components/Space";
+import Body from "./components/Body";
 import Colors from './components/Colors';
 import Div from './components/Div';
+import Space from "./components/Space";
 import image from "./images/12524041_10153845708099354_6990004252861025374_n.jpg";
 
 render(
   <Router>
+    <Body margin={0}>
     <Div row style={{position: "absolute"}}>
       <Div><Link to="/"><img src={image} width={40} height={40}/></Link></Div>
     </Div>
@@ -17,6 +19,7 @@ render(
       <Route path="/space" exact component={Space} />
       <Route path="/colors" exact component={Colors} />
     </Div>
+    </Body>
   </Router>,
   document.getElementById("app")
 );
