2b8efffebcdc0a1e99d30ead0810d81a1035ff2b
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 21:02:33 -0700

    src/components/Div.js

--- src/components/Div.js
+++ src/components/Div.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default ({ centered, children, row, style }) => (
+export default ({ centered, children, flex, row, style }) => (
   <div
     style={{
       alignItems: centered && "center",
       display: "flex",
+      flex,
       flexDirection: row ? "row" : "column",
       justifyContent: centered && "center",
       margin: 4,
