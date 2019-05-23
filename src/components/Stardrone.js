48fcf131f6525d072cfcf8abe762865e90e379e4
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 21:06:19 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -3,7 +3,7 @@ import Div from "./Div";
 import { Link } from "react-router-dom";
 
 export default () => (
-  <Div centered flex={1} style={{ marginLeft: 60 }}>
+  <Div centered flex={1}>
     <Div><Link to="space">Space</Link></Div>
   </Div>
 );
