a3d2e048fb20f460d71d0929d373c73a0924f005
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 23:23:40 -0700

    src/components/Music.js

--- src/components/Music.js
+++ src/components/Music.js
@@ -2,5 +2,5 @@ import React from 'react';
 
 export default () =>
   <div>
-    <iframe style={{}} src="https://www.youtube.com/embed?listType=playlist&modestbranding=1&list=PLVnJFr0ragjfug9bOIVlZHzjEdceWcgws&autoplay=1" title="music" style={iframeStyle} frameBorder={0} width="560" height="315" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    <iframe src="https://www.youtube.com/embed?listType=playlist&modestbranding=1&list=PLVnJFr0ragjfug9bOIVlZHzjEdceWcgws&autoplay=1" title="music" style={iframeStyle} frameBorder={0} width="560" height="315" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
   </div>
