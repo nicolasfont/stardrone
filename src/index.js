cf4dd61f8fc8f0682c559e2aa7d83d3d427b42e3
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 20:22:12 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Stardrone from "./components/Stardrone";
-import ExperimentInSpace from "./components/ExperimentInSpace";
 import Space from './components/Space';
 
 render(
