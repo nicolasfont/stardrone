1163b8d132abb418775696d5c418e755508f889d
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 20:06:56 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { render } from "react-dom";
+import React, { render } from "react-dom";
 import ExperimentInSpace from './components/ExperimentInSpace';
 
 render(<ExperimentInSpace />, document.getElementById("app"));
