7d4c10ddde881673c0f8cac651e07295c4a84f0a
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 20:13:37 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import ExperimentInSpace from './components/ExperimentInSpace';
+import ExperimentInSpace from './ExperimentInSpace';
 
 export default () =>
   <Router>
