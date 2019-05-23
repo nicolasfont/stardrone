d88f6ac248507d22b6b3ca3b850c7b27d10eb112
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 21:30:41 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import image from "./images/12524041_10153845708099354_6990004252861025374_n.jpg
 render(
   <Router>
     <Body margin={0}>
-      <Div flex={1}>
         <Div style={{ position: "absolute" }}>
           <Link to="/">
             <img src={image} width={40} height={40} />
@@ -22,7 +21,6 @@ render(
           <Route path="/space" exact component={Space} />
           <Route path="/colors" exact component={Colors} />
         </Div>
-      </Div>
     </Body>
   </Router>,
   document.getElementById("app")
