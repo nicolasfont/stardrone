eb26bd6bc3dc322df95392d831d6068c4d3faf24
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 20:05:54 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import hash from "object-hash";
-import React, { createContext, useContext, useState } from "react";
 import { render } from "react-dom";
 import ExperimentInSpace from './components/ExperimentInSpace';
 
