435e6b2e51b575492dd03a8645e5099d8e2f34ea
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 20:15:21 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Stardrone from './components/Stardrone';
 import ExperimentInSpace from './components/ExperimentInSpace';
 
-render(<Router>
-  <Route path="/" exact component={ExperimentInSpace}/>
-</Router>, document.getElementById("app"));
+render(
+  <Router>
+    <Route path="/" exact component={ExperimentInSpace}/>
+  </Router>
+  , document.getElementById("app"));
