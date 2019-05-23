9b86aca83f9049aefb75ddd56809bebce5f7f5dc
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 20:44:57 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Div from "./Div";
+import { Link } from "react-router-dom";
 
 export default () => (
   <Div>
     Stardrone
+    <Div><Link to="space">Space</Link></Div>
   </Div>
 );
