d92b25c7428ede1a1f739ef329e602b6bbfbc479
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 20:26:33 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const Div = ({ children }) =>
+  <div style={{
+    padding: 4,
+    margin: 4
+  }}>{children}</div>;
+
 export default () => (
   <div>
     <div>Stardrone</div>
