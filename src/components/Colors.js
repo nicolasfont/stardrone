7199c8d2789ff116955a5305a3d8e8a1b049632d
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 23:39:42 -0700

    src/components/Colors.js

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -4,10 +4,10 @@ import { Motion, spring } from 'react-motion';
 
 export default () =>
   <Div row>
-      <Motion defaultStyle={{ width: 0 }} style={{ width: spring(30) }}>
-        {({ width }) =>
+      <Motion defaultStyle={{ height: 0, width: 0 }} style={{ height: spring(30), width: spring(30) }}>
+        {({ height, width }) =>
           <>
-            <Div style={{ backgroundColor: "red", height: width, width: width }}></Div>
+            <Div style={{ backgroundColor: "red", height, width }}></Div>
             <Div style={{ backgroundColor: "purple" }}></Div>
             <Div style={{ backgroundColor: "blue" }}></Div>
             <Div style={{ backgroundColor: "lightblue" }}></Div>
