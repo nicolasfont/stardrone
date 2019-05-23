9005a91dd8e4f4fc8a26461cd84055aad5986ddc
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 20:27:06 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -8,7 +8,7 @@ const Div = ({ children }) =>
   }}>{children}</div>;
 
 export default () => (
-  <Div column>
+  <Div row>
     <Div>Stardrone</Div>
     <Div><Link to="space">Space</Link></Div>
   </Div>
