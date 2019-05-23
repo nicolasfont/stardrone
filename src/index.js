f40db77c3608a8a6012800356141a1c8803ca9da
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 20:13:42 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { render } from "react-dom";
 import ExperimentInSpace from './components/ExperimentInSpace';
 import Stardrone from './components/Stardrone';
 
-render(<ExperimentInSpace />, document.getElementById("app"));
+render(<Stardrone />, document.getElementById("app"));
