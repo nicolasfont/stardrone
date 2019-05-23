6bfa84f8623ada40c56a74db6587ecbb6fa4766c
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 21:19:10 -0700

    src/components/Body.js

---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+export default ({ children, margin }) => {
+  useEffect(() => {
+    document.body.style.margin = margin;
+    return () => {
+      document.body.style.margin = null;
+    };
+  }, [margin]);
+  return children;
+};
