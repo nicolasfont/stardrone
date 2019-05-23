ea7fbbd5cb3cdd9b96ce7fdd0b81315204eac89f
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 20:33:15 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -4,25 +4,30 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Stardrone from "./components/Stardrone";
 import Space from "./components/Space";
 
-const Div = ({ children, row }) =>
-  <div style={{
-    display: "flex",
-    flexDirection: row ? "row" : "column",
-    margin: 4,
-    padding: 4,
-  }}>{children}</div>;
+const Div = ({ children, row }) => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: row ? "row" : "column",
+      margin: 4,
+      padding: 4
+    }}
+  >
+    {children}
+  </div>
+);
 
 render(
-      <Router>
-  <Div>
-    <Div row>
-      <Link to="space">Space</Link>
-    </Div>
+  <Router>
     <Div>
+      <Div row>
+        <Link to="space">Space</Link>
+      </Div>
+      <Div>
         <Route path="/" exact component={Stardrone} />
         <Route path="/space" exact component={Space} />
+      </Div>
     </Div>
-  </Div>
-      </Router>,
+  </Router>,
   document.getElementById("app")
 );
