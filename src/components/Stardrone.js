2f023cc504262568d510583f45accd2507727b16
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 20:30:17 -0700

    src/components/Stardrone.js src/index.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -1,17 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Div = ({ children }) =>
-  <div style={{
-    display: "flex",
-    flexDirection: "row",
-    margin: 4,
-    padding: 4,
-  }}>{children}</div>;
-
 export default () => (
-  <Div>
-    <Div>Stardrone</Div>
-    <Div><Link to="space">Space</Link></Div>
-  </Div>
+  <div>
+    <div>Stardrone</div>
+    <div><Link to="space">Space</Link></div>
+  </div>
 );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Stardrone from "./components/Stardrone";
 import Space from "./components/Space";
 
+
+const Div = ({ children }) =>
+  <div style={{
+    display: "flex",
+    flexDirection: "row",
+    margin: 4,
+    padding: 4,
+  }}>{children}</div>;
+
 render(
   <Router>
     <Route path="/" exact component={Stardrone} />
