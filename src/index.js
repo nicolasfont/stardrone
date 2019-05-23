b9c058b0e325755770e7b5940594df41ca88fc93
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 21:25:15 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ render(
       </Div>
       <Div centered flex={1}>
         <Route path="/" exact component={Stardrone} />
-        <Route path="space" exact component={Space} />
-        <Route path="colors" exact component={Colors} />
+        <Route path="/space" exact component={Space} />
+        <Route path="/colors" exact component={Colors} />
       </Div>
     </Body>
   </Router>,
