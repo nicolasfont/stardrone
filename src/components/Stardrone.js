3c668f925d8b1701555c66e23f64bbece04c1f75
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 20:27:45 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Div = ({ children }) =>
   <div style={{
-    flexDirection: "column",
+    flexDirection: "row",
     margin: 4,
     padding: 4,
   }}>{children}</div>;
