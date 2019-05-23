1f10a37aa299b740b6b459070f98742a1df299f0
Nicolás Font <nicolasfont@gmail.com>
2019-05-22 21:53:21 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -2,12 +2,13 @@ import React from "react";
 import Div from "./Div";
 import { Link as BaseLink } from "react-router-dom";
 
-const Link = ({ children }) => <BaseLink>{children}</BaseLink>;
+const Link = ({ children }) =>
+  <BaseLink>{children}</BaseLink>;
 
 export default () => (
   <Div>
     <Div>
-      <Link style={{ textDecoration: "none" }} to="space">Space</Link>
+      <Link to="space">Space</Link>
     </Div>
     <Div>
       <Link to="colors">Colors</Link>
