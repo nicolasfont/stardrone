f9e0e16b099be090ec8a02a41aa1091e9066db5b
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 23:48:19 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -24,7 +24,6 @@ export default () => (
   <Div centered flex={1}>
     <Div>
       <Link to="/"><Div>The Stardrones</Div></Link>
-      <Link to="/"><Div>Les Trous Noirs N'Existent Pas</Div></Link>
     </Div>
     <Div centered>
       <img src={image} width={250} />
