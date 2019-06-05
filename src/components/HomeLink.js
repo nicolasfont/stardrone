8affdf1cf326f8b4a03b81f43fb2cc151e53c774
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 21:33:58 -0700

    src/components/HomeLink.js

--- src/components/HomeLink.js
+++ src/components/HomeLink.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Div from "./Div";
-import TheStardrones from "./components/TheStardrones";
 import image from "./images/12524041_10153845708099354_6990004252861025374_n.jpg";
-import "./fonts/billy-argel_amsterdam/Amsterdam Personal Use.ttf";
 
 export default ({ style }) => (
   <Div style={{ position: "absolute", ...style }}>
