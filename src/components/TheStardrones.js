e7ead27d9b06e3fd77839ade99ee55e034a52126
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 22:44:44 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Div from "./Div";
 import HomeLink from "./HomeLink";
-import image from "../images/TheStardrones";
+import image from "../images/TheStardrones.jpg";
 import { Link as BaseLink } from "react-router-dom";
 
 const Link = ({ children, ...props }) => (
