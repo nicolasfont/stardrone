26fbdabb169e132a8d4877f391d022aa67f91afe
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 23:34:52 -0700

    src/components/Music.js

--- src/components/Music.js
+++ src/components/Music.js
@@ -4,7 +4,7 @@ import HomeLink from "./HomeLink";
 export default () => (
   <div>
     <iframe
-      src="https://www.youtube.com/embed?listType=playlist&modestbranding=1&list=PLVnJFr0ragjfug9bOIVlZHzjEdceWcgws&autoplay=1"
+      src="https://www.youtube.com/embed?listType=playlist&modestbranding=1&list=PLVnJFr0ragjfug9bOIVlZHzjEdceWcgws&autoplay=0"
       title="music"
       frameBorder={0}
       width="560"
