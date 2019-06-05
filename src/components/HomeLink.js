eba759fa0bc09845c3bc04c22b9f3d7248aab494
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 21:38:48 -0700

    src/components/HomeLink.js

--- src/components/HomeLink.js
+++ src/components/HomeLink.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Div from "./Div";
 import image from "../images/12524041_10153845708099354_6990004252861025374_n.jpg";
-import { Link as BaseLink } from "react-router-dom";
+import { Link BaseLink } from "react-router-dom";
 
 export default ({ style }) => (
   <Div style={{ position: "absolute", ...style }}>
