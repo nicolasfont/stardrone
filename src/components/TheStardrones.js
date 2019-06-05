9402fe98d21df67da53783d185e6bf33b31f6e13
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 22:06:34 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -8,7 +8,7 @@ const Link = ({ children, ...props }) => (
     <BaseLink
       style={{
         color: "black",
-        fontFamily: "Black Diamonds",
+        fontFamily: "Amsterdam",
         textDecoration: "none"
       }}
       {...props}
