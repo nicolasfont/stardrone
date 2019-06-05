f899082664ca1924b2e91e5a16915397a6ea80ee
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 21:29:28 -0700

    src/components/HomeLink.js

--- src/components/HomeLink.js
+++ src/components/HomeLink.js
@@ -11,7 +11,7 @@ import TheStardrones from "./components/TheStardrones";
 import image from "./images/12524041_10153845708099354_6990004252861025374_n.jpg";
 import "./fonts/billy-argel_amsterdam/Amsterdam Personal Use.ttf";
 
-export default () => (
+export default ({ style }) => (
   <Div style={{ position: "absolute" }}>
     <Link to="/">
       <img src={image} width={40} height={40} style={{ borderRadius: "50%" }} />
