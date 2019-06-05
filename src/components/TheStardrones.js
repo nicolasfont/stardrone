12430d16fe8b2d4c493eaff9761d757cd1bbd058
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 22:03:50 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -1,7 +1,4 @@
-import React from "react";
-import Div from "./Div";
-import HomeLink from "./HomeLink";
-import { Link as BaseLink } from "react-router-dom";
+import React from "react"; import Div from "./Div"; import HomeLink from "./HomeLink"; import { Link as BaseLink } from "react-router-dom";
 
 const Link = ({ children, ...props }) => (
   <Div>
