df4e17cbd55e0a0cbd318da263f753928f8cba73
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 22:46:20 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -25,7 +25,7 @@ export default () => (
       <Link to="/">The Stardrones</Link>
     </Div>
     <Div centered>
-      <img src={image} width={200} />
+      <img src={image} width={200} style={{ borderRadius: "50%" }} />
     </Div>
   </Div>
 );
