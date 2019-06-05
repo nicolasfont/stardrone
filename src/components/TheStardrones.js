50a4d6f4dee66a58d140a793182c8fa2ca2b285f
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 22:51:17 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -22,6 +22,9 @@ const Link = ({ children, ...props }) => (
 export default () => (
   <Div centered flex={1}>
     <Div>
+      <Link to="/">The Stardrones</Link>
+    </Div>
+    <Div>
       <img src={image} width={250} />
     </Div>
   </Div>
