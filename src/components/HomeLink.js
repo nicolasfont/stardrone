12b6ee289f5c5a0c29410e92ecced33c72126b4a
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 21:37:15 -0700

    src/components/HomeLink.js

--- src/components/HomeLink.js
+++ src/components/HomeLink.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Div from "./Div";
 import image from "../images/12524041_10153845708099354_6990004252861025374_n.jpg";
-p
 
 export default ({ style }) => (
   <Div style={{ position: "absolute", ...style }}>
