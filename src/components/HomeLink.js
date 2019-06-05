e69ca16bac8cb24e9908c8a641e86f5c52df0b5e
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 21:37:10 -0700

    src/components/HomeLink.js

--- src/components/HomeLink.js
+++ src/components/HomeLink.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Div from "./Div";
 import image from "../images/12524041_10153845708099354_6990004252861025374_n.jpg";
-import { Link } from "react-router-dom";
+p
 
 export default ({ style }) => (
-  <Div style={{ position: "absolute" }}>
+  <Div style={{ position: "absolute", ...style }}>
     <Link to="/">
       <img src={image} width={40} height={40} style={{ borderRadius: "50%" }} />
     </Link>
