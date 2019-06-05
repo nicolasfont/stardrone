e21f3e848cdbd62521deee8f1602a137e73f59b3
Nicolás Font <nicolasfont@gmail.com>
2019-06-04 21:26:42 -0700

    src/components/HomeLink.js

--- src/components/HomeLink.js
+++ src/components/HomeLink.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Body from "./components/Body";
 import Div from "./components/Div";
 import image from "./images/12524041_10153845708099354_6990004252861025374_n.jpg";
 
