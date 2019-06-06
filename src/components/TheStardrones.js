b6ba443b8d15a9c95e1562480482fb23b5339fa4
Nicolás Font <nicolasfont@gmail.com>
2019-06-05 22:15:29 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -6,33 +6,32 @@ import image from "../images/TheStardrones.jpg";
 import { Link as BaseLink } from "react-router-dom";
 
 const Link = ({ children, ...props }) => (
-    <BaseLink
-      style={{
-        color: "black",
-        fontFamily: "Amsterdam",
-        textDecoration: "none",
-        fontSize: "12px",
-        padding: 0,
-        margin: 0
-      }}
-      {...props}
-    >
-      {children}
-    </BaseLink>
+  <BaseLink
+    style={{
+      color: "black",
+      fontFamily: "Amsterdam",
+      textDecoration: "none",
+      fontSize: "12px",
+      padding: 0,
+      margin: 0
+    }}
+    {...props}
+  >
+    {children}
+  </BaseLink>
 );
 
 export default () => (
   <Div centered flex={1}>
-    <Div border style={{ padding: 0, margin: 0}}>
-    <Div centered>
-      <img src={image} width={250} />
+    <Div border style={{ padding: 0, margin: 0 }}>
+      <Div centered>
+        <img src={image} width={250} />
+      </Div>
+      <Div>
+        <Link to="/">The Stardrones</Link>
+        <Link to="/">Les Trous Noirs N'Existent Pas</Link>
+      </Div>
+      <Div />
     </Div>
-    <Div>
-      <Link to="/">The Stardrones</Link>
-      <Link to="/">Les Trous Noirs N'Existent Pas</Link>
-    </Div>
-    <Div>
-    </Div>
-  </Div>
   </Div>
 );
