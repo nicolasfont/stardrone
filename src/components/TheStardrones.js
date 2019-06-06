01a74d0045f7fbee67b51792b6e16c54a0d0411f
Nicolás Font <nicolasfont@gmail.com>
2019-06-05 22:24:14 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -11,7 +11,7 @@ const Link = ({ children, small, ...props }) => (
       color: "black",
       fontFamily: "Amsterdam",
       textDecoration: "none",
-      fontSize: small ? "10px" : "12px",
+      fontSize: small ? "10px" : "14px",
       padding: 0,
       margin: 0
     }}
