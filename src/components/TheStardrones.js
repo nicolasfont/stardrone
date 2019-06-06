ee8bb6eebc59d1022833cb84c8ce2893e35b8150
Nicolás Font <nicolasfont@gmail.com>
2019-06-05 22:24:51 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -25,7 +25,7 @@ const size = 250;
 
 export default () => (
   <Div centered flex={1}>
-    <Div centered style={{ width: 500, height: 400 }}>
+    <Div centered style={{ width: 500, height: 500 }}>
       <Div centered flex={1}>
         <img src={image} width={size} height={size} />
       </Div>
