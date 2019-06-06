0bd0772d974adc321ddcbe4556694fa5e24e269e
Nicolás Font <nicolasfont@gmail.com>
2019-06-05 22:18:02 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -29,7 +29,7 @@ export default () => (
       <Div centered style={{ padding: 0, margin: 0 }}>
         <img src={image} width={size} height={size} />
       </Div>
-      <Div style={{ padding: 0, margin: 0 }}>
+      <Div centered style={{ padding: 0, margin: 0 }}>
         <Link to="/">The Stardrones</Link>
         <Link to="/">Les Trous Noirs N'Existent Pas</Link>
       </Div>
