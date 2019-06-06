e724d50286f91c8acd8328dc3d0894bad4b5a40d
Nicolás Font <nicolasfont@gmail.com>
2019-06-05 22:13:25 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -28,7 +28,7 @@ export default () => (
     </Div>
     <Div>
       <Link to="/">The Stardrones</Link>
-      <Link to="/">les trous noirs n'existent pas</Link>
+      <Link to="/">Les trous noirs n'existent pas</Link>
     </Div>
     <Div>
     </Div>
