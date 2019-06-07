1748421234d27e0ddd9d6567965ba17ba41f5d90
Nicolás Font <nicolasfont@gmail.com>
2019-06-06 21:44:44 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -26,11 +26,6 @@ export default () => (
     <Div centered row flex={1}>
       <Link to="/">Something like a spaceship</Link>
     </Div>
-    <Div centered row flex={1}>
-      <Link to="music">Music</Link>
-      <Link to="colors">Colors</Link>
-      <Link to="space">Space</Link>
-    </Div>
     <Div centered flex={1}>
       <Div centered>
         <Link to="/">Something like a spaceship</Link>
