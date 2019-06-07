8a8b2afee7996a4326a8d917fb800f5c6ac25721
Nicolás Font <nicolasfont@gmail.com>
2019-06-06 23:25:13 -0700

    src/components/Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 
 export default ({ children, margin, title }) => {
   useEffect(() => {
+    
     document.title = title || "Stardrone";
     document.body.style.margin = margin;
     return () => {
