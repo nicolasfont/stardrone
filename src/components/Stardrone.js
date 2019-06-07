0819a770569baa3b6594a6c4f309ba126ded2eec
Nicolás Font <nicolasfont@gmail.com>
2019-06-06 21:42:36 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -28,6 +28,11 @@ export default () => (
       <Link to="colors">Colors</Link>
       <Link to="space">Space</Link>
     </Div>
+    <Div centered row flex={1}>
+      <Link to="music">Music</Link>
+      <Link to="colors">Colors</Link>
+      <Link to="space">Space</Link>
+    </Div>
     <Div centered flex={1}>
       <Div centered>
         <Link to="space">les trous noirs</Link>
