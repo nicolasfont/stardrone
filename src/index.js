deacf108befb9efc826e8613c585579fc92295fb
Nicolás Font <nicolasfont@gmail.com>
2019-06-08 00:14:26 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Music from "./components/Music";
 import Space from "./components/Space";
 import Stardrone from "./components/Stardrone";
 import TheStardrones from "./components/TheStardrones";
+import NicolasFont from './components/NicolasFont";
 import image from "./images/12524041_10153845708099354_6990004252861025374_n.jpg";
 import "./fonts/billy-argel_amsterdam/Amsterdam Personal Use.ttf";
 import "./fonts/Black Diamonds Personal Use.ttf";
