65688905295e5748b5e45f32fae5a86b24df1ad1
Nicolás Font <nicolasfont@gmail.com>
2019-06-08 15:30:47 -0700

    src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Body from "./Body";
