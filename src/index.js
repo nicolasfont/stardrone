cb932c178f871cc3b520e7da79956a56b08bce84
Nicolás Font <nicolasfont@gmail.com>
2019-06-08 00:25:14 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -27,7 +27,6 @@ render(
           <Route path="/thestardrones" exact component={TheStardrones} />
         </Div>
       </Div>
-      <audio autoPlay loop src={soundtrack} />
     </Body>
   </Router>,
   document.getElementById("app")
