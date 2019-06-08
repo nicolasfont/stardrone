6c776f67d39e1aaf31b7514d447df06b63060237
Nicolás Font <nicolasfont@gmail.com>
2019-06-08 16:52:29 -0700

    src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -15,15 +15,24 @@ import "../fonts/Black Diamonds Personal Use.ttf";
 import soundtrack from "../sounds/The Stardrones - The Tremecula Dance.mp3";
 
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+const source = audioCtx.createBufferSource();
+
+audioCtx.decodeAudioData(soundtrack, buffer => {
+  source.buffer = buffer;
+  source.connect(audioCtx.destination);
+  source.loop = true;
+}, error => console.log(error));
 
 export default () => {
   const audioRef = useRef(null);
   const [playing, play] = useState(false);
   useEffect(() => {
     if (playing) {
-      audioRef.current.play();
+      source.resume();
+      // audioRef.current.play();
     } else {
-      audioRef.current.pause();
+      source.pause();
+      // audioRef.current.pause();
     }
   }, [playing]);
   return (
