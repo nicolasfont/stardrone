78c28801ac7822a00982c236765b76b717b6369c
Nicolás Font <nicolasfont@gmail.com>
2019-06-08 00:18:28 -0700

    src/components/NicolasFont.js

--- src/components/NicolasFont.js
+++ src/components/NicolasFont.js
@@ -2,4 +2,4 @@ import React from 'react';
 import Div from './Div';
 
 export default () =>
-  <Div centered style={{ fontFamily: "Amsterdam", fontSize: 16 }}>Who the fuck is Nicolás Font?</Div>;
+  <Div centered flex={1} style={{ fontFamily: "Amsterdam", fontSize: 16 }}>Who the fuck is Nicolás Font?</Div>;
