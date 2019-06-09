b9e889f821067c0a93abd8a167321d14b6583b8b
Nicolás Font <nicolasfont@gmail.com>
2019-06-09 00:20:48 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -26,8 +26,8 @@ export default () => (
       <HomeLink />
       <Div centered flex={1}>
         <Div centered>
-          <Link to="/thestardrones">The Tremecula Dance</Link>
           <Link to="/thestardrones">Something Like A Spaceship</Link>
+          <Link to="/thestardrones">The Tremecula Dance</Link>
           <Link to="/thestardrones">The Gazing Hour</Link>
           <Link to="/thestardrones">Here To Fuck Shit Up</Link>
           <Link to="/thestardrones">Les Trous Noirs N'Existent Pas</Link>
