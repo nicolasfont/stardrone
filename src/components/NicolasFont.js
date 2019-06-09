4ab673bd744130044565580392b43bc6c163b770
Nicolás Font <nicolasfont@gmail.com>
2019-06-09 01:19:03 -0700

    src/components/NicolasFont.js

--- src/components/NicolasFont.js
+++ src/components/NicolasFont.js
@@ -4,7 +4,6 @@ import HomeLink from "./HomeLink";
 
 export default () => (
   <Div centered flex={1} style={{ fontFamily: "Amsterdam", fontSize: 16 }}>
-      Lkalal
     <HomeLink />
     Who the fuck is Nicolás Font
   </Div>
