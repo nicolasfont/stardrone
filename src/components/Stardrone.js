a8374c3bc611dd10d38f6b99743378f0245bbfe7
Nicolás Font <nicolasfont@gmail.com>
2019-06-09 00:26:35 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -24,21 +24,21 @@ export default ({ small }) => (
   <Body title="Stardrone">
     <Div centered flex={1}>
       <HomeLink />
-        <Link small={small} to="/thestardrones">
-          Something Like A Spaceship
-        </Link>
-        <Link small={small} to="/thestardrones">
-          The Tremecula Dance
-        </Link>
-        <Link small={small} to="/thestardrones">
-          The Gazing Hour
-        </Link>
-        <Link small={small} to="/thestardrones">
-          Here To Fuck Shit Up
-        </Link>
-        <Link small={small} to="/thestardrones">
-          Les Trous Noirs N'Existent Pas
-        </Link>
+      <Link small={small} to="/thestardrones">
+        Something Like A Spaceship
+      </Link>
+      <Link small={small} to="/thestardrones">
+        The Tremecula Dance
+      </Link>
+      <Link small={small} to="/thestardrones">
+        The Gazing Hour
+      </Link>
+      <Link small={small} to="/thestardrones">
+        Here To Fuck Shit Up
+      </Link>
+      <Link small={small} to="/thestardrones">
+        Les Trous Noirs N'Existent Pas
+      </Link>
     </Div>
   </Body>
 );
