fa9ef9b2a2383aa57d53005c4fdba937e458023d
Nicolás Font <nicolasfont@gmail.com>
2019-06-08 18:22:13 -0700

    src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ import soundtrack from "../sounds/The Stardrones - The Tremecula Dance.mp3";
 
 const myAudio = new Audio(soundtrack);
 myAudio.ontimeupdate = function(i) {
-  if (this.currentTime >= this.duration - 0.44) {
+  if (this.currentTime >= this.duration - 0.1) {
     this.currentTime = 0;
     this.play();
   }
