226aa0664a6eafe0b92f1719239c21b52503456c
Nicolás Font <nicolasfont@gmail.com>
2019-06-09 23:31:54 -0700

    src/components/Space.js

--- src/components/Space.js
+++ src/components/Space.js
@@ -86,7 +86,7 @@ const Body = ({ children }) => {
   }, [color]);
 
   return (
-    <Div noBorder flex={1} style={{ maxWidth: 650, margin: "auto" }}>
+    <Div noBorder flex={1} style={{ maxWidth: 655, margin: "auto" }}>
       {children}
     </Div>
   );
