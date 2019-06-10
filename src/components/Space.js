bc3d30d4f1b1f4ac92f8e4f5b1dc8ae99894d83e
Nicolás Font <nicolasfont@gmail.com>
2019-06-09 23:43:24 -0700

    src/components/Space.js

--- src/components/Space.js
+++ src/components/Space.js
@@ -8,7 +8,7 @@ const ColorContext = createContext();
 const ColorProvider = ({ children }) => {
   const [base, setBase] = useState("");
   const seed = "";
-  const hue = parseInt(hash(seed + base), 16) % 360;
+  const hue = parseInt(hash(base), 16) % 360;
   const hue2 = (hue + 36) % 360;
   const saturation = `${80}%`;
   const lightness = `${70}%`;
