0211f1d1017d70b6a326a06bac23cea8edf5c347
Nicolás Font <nicolasfont@gmail.com>
2019-06-10 22:44:30 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -2,7 +2,6 @@ import React from "react";
 import Body from "./Body";
 import Div from "./Div";
 import HomeLink from "./HomeLink";
-import { Link as BaseLink } from "react-router-dom";
 
 export default () => (
   <Body title="Stardrone">
