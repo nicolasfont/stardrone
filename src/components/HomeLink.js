5607e824f09cda194a95e777606fb816214b92f2
Nicolás Font <nicolasfont@gmail.com>
2019-06-11 08:09:02 -0700

    src/components/HomeLink.js src/components/TheStardrones.js

--- src/components/HomeLink.js
+++ src/components/HomeLink.js
@@ -4,13 +4,13 @@ import image from "../images/12524041_10153845708099354_6990004252861025374_n.jp
 import { Link } from "react-router-dom";
 
 export default ({ title, to }) => (
-    <Link to={to || "/thestardrones"}>
-      <img
-        title={title || "The Stardrones"}
-        src={image}
-        width={40}
-        height={40}
-        style={{ position: "absolute", left: 40, top: 40, borderRadius: "50%" }}
-      />
-    </Link>
+  <Link to={to || "/thestardrones"}>
+    <img
+      title={title || "The Stardrones"}
+      src={image}
+      width={40}
+      height={40}
+      style={{ position: "absolute", left: 40, top: 40, borderRadius: "50%" }}
+    />
+  </Link>
 );
--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -28,7 +28,11 @@ const License = () => (
     style={{ position: "absolute", right: 40, bottom: 40 }}
     title="This work is licensed under a Creative Commons Attribution 4.0 International License."
   >
-    <a rel="license" href="http://creativecommons.org/licenses/by/4.0/" target="_blank">
+    <a
+      rel="license"
+      href="http://creativecommons.org/licenses/by/4.0/"
+      target="_blank"
+    >
       <img
         alt="Creative Commons License"
         style={{ borderWidth: 0 }}
