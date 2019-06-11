9e3e28a5ad72f9b358d4f91fe65c29cad4c6c675
Nicolás Font <nicolasfont@gmail.com>
2019-06-10 22:41:59 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -58,7 +58,24 @@ export default ({ playing, play }) => {
       <Div centered flex={1}>
           <Div onClick={flip} style={{ cursor: "pointer" }}>
         {flipped ? (
-          <Div>back</Div>
+          <Div>
+<Link small={small} to="/thestardrones">
+          Something Like A Spaceship
+        </Link>
+        <Link small={small} to="/thestardrones">
+          The Tremecula Dance
+        </Link>
+        <Link small={small} to="/thestardrones">
+          The Gazing Hour
+        </Link>
+        <Link small={small} to="/thestardrones">
+          Here To Fuck Shit Up
+        </Link>
+        <Link small={small} to="/thestardrones">
+          Les Trous Noirs N'Existent Pas
+        </Link>
+
+          </Div>
         ) : (
           <>
             <Div style={{ padding: 16 }}>
