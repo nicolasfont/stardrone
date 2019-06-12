8d2175faacf5a38ecd7f26c6c7900cebc81eadbc
Nicolás Font <nicolasfont@gmail.com>
2019-06-11 22:13:54 -0700

    src/components/See.js

--- src/components/See.js
+++ src/components/See.js
@@ -2,6 +2,9 @@ import React from "react";
 import Div from "./Div";
 
 export default () => {
-  return <Div centered flex={1}>See</Div>;
+  return (
+    <Div centered flex={1}>
+      See
+    </Div>
+  );
 };
-
