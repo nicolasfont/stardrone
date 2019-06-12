ca3a981e60ae11b7e5f81aa8af0037564f63ac98
Nicolás Font <nicolasfont@gmail.com>
2019-06-11 22:14:47 -0700

    src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Body from "./Body";
 import Colors from "./Colors";
 import Div from "./Div";
 import Music from "./Music";
+import See from "./See";
 import Space from "./Space";
 import Stardrone from "./Stardrone";
 import TheStardrones from "./TheStardrones";
@@ -53,10 +54,11 @@ export default () => {
         <Div flex={1}>
           <Div flex={1}>
             <Route path="/" exact component={() => <Stardrone />} />
-            <Route path="/space" exact component={Space} />
             <Route path="/colors" exact component={Colors} />
             <Route path="/music" exact component={Music} />
             <Route path="/nicolasfont" exact component={NicolasFont} />
+            <Route path="/see" exact component={See} />
+            <Route path="/space" exact component={Space} />
             <Route
               path="/thestardrones"
               exact
