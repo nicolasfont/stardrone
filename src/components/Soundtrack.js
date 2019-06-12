8b8fd371c42f44af11dac92ec09317a1d17fbd92
Nicolás Font <nicolasfont@gmail.com>
2019-06-11 23:12:17 -0700

    src/components/Soundtrack.js

--- src/components/Soundtrack.js
+++ src/components/Soundtrack.js
@@ -1,17 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Body from "./Body";
-import Colors from "./Colors";
-import Div from "./Div";
-import Music from "./Music";
-import See from "./See";
-import Space from "./Space";
-import Stardrone from "./Stardrone";
-import TheStardrones from "./TheStardrones";
-import NicolasFont from "./NicolasFont";
-import image from "../images/12524041_10153845708099354_6990004252861025374_n.jpg";
-import "../fonts/billy-argel_amsterdam/Amsterdam Personal Use.ttf";
 
 const context = new AudioContext();
 const source = context.createBufferSource();
