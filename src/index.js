51fc334246a87c4505a84d42fb9c8b7a041cb8ac
Nicolás Font <nicolasfont@gmail.com>
2019-06-12 00:26:14 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import App from "./components/App";
-import Body from "./components/Body";
-import Colors from "./components/Colors";
-import Div from "./components/Div";
-import Music from "./components/Music";
-import Space from "./components/Space";
-import Stardrone from "./components/Stardrone";
-import TheStardrones from "./components/TheStardrones";
-import NicolasFont from "./components/NicolasFont";
-import image from "./images/12524041_10153845708099354_6990004252861025374_n.jpg";
-import "./fonts/billy-argel_amsterdam/Amsterdam Personal Use.ttf";
 
 render(<App />, document.getElementById("app"));
