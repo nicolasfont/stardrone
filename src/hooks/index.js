ca3241a2535f769534012fa3f2ec0c55a7e7c501
Nicolás Font <nicolasfont@gmail.com>
2019-06-11 22:57:16 -0700

    src/hooks/index.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -4,3 +4,9 @@ export const useToggle = initialState => {
   const [toggle, setToggle] = useState(initialState);
   return [toggle, () => setToggle(!toggle)];
 };
+
+export const useRotation = initialStates => {
+  const [index, setIndex] = useState(0);
+  return [initialStates && initialStates[index], () => setIndex(index + 1)];
+};
+
