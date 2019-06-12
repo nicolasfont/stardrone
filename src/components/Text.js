5cdcbbf40c2e9e8641a94fd534bbb3f2c5d68710
Nicolás Font <nicolasfont@gmail.com>
2019-06-11 22:18:34 -0700

    src/components/Text.js

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,4 +1,5 @@
 import React from "react";
 
-export default ({ children }) =>
-  <span style={{ fontFamily: "Amsterdam", fontSize: 16 }}>{children}</span>;
+export default ({ children }) => (
+  <span style={{ fontFamily: "Amsterdam", fontSize: 16 }}>{children}</span>
+);
