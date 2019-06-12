7e1dcbcc07ab53edf2c186c5e9ee037460fa11f0
Nicolás Font <nicolasfont@gmail.com>
2019-06-11 23:14:36 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -68,7 +68,7 @@ const useToggle = initialState => {
 };
 
 export default () => {
-  const [flipped, rotate] = useRotation([0, 1, 2]);
+  const [rotation, rotate] = useRotation([0, 1, 2]);
   return (
     <Body title="The Stardrones">
       <HomeLink
@@ -78,15 +78,14 @@ export default () => {
       <License />
       <Div centered flex={1}>
         <Div onClick={rotate} style={{ cursor: "pointer" }}>
-          {flipped ? (
+            [
             <Div>
               <Link to="/thestardrones">The Tremecula Dance</Link>
               <Link to="/thestardrones">Something Like A Spaceship</Link>
               <Link to="/thestardrones">Les Trous Noirs N'Existent Pas</Link>
               <Link to="/thestardrones">Here To Fuck Shit Up</Link>
               <Link to="/thestardrones">The Gazing Hour</Link>
-            </Div>
-          ) : (
+            </Div>,
             <>
               <Div style={{ padding: 16 }}>
                 <img
@@ -99,7 +98,8 @@ export default () => {
               <Div style={{ padding: 16 }}>
                 <Link style={{ fontSize: "1.3rem" }}>The Stardrones</Link>
               </Div>
-            </>
+              </>,
+              <Div centered flex={1}>the universe is not real and it's ok</Div>
           )}
         </Div>
       </Div>
