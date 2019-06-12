91d4fb6274fcba10ac84113878c1704de2b1eff8
Nicolás Font <nicolasfont@gmail.com>
2019-06-11 23:28:01 -0700

    src/components/HomeLink.js

--- src/components/HomeLink.js
+++ src/components/HomeLink.js
@@ -3,7 +3,7 @@ import Div from "./Div";
 import image from "../images/12524041_10153845708099354_6990004252861025374_n.jpg";
 import { Link } from "react-router-dom";
 
-export default ({ title, to, postition }) => (
+export default ({ postition, title, to }) => (
   <Link to={to || "/thestardrones"}>
     <img
       title={title || "The Stardrones"}
