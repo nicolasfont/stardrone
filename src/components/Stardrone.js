9ebdc8113fbd8df02c77eb476f7bd90497fe4ce2
Nicolás Font <nicolasfont@gmail.com>
2019-06-11 23:26:59 -0700

    src/components/Stardrone.js

--- src/components/Stardrone.js
+++ src/components/Stardrone.js
@@ -13,7 +13,7 @@ export default () => (
       </Div>
       <Div>
         <Text>
-          Music, film, visual, and code by Nicolás Font except where noted.
+          Music, film, visual, and code by Nicolás Font except where noted
         </Text>
       </Div>
     </Div>
