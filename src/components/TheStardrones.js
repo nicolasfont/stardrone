3e186c130971f38b662833ef453bb30c5d3c17f8
Nicolás Font <nicolasfont@gmail.com>
2019-06-11 23:35:51 -0700

    src/components/TheStardrones.js

--- src/components/TheStardrones.js
+++ src/components/TheStardrones.js
@@ -26,25 +26,6 @@ const Link = ({ children, style, ...props }) => (
   </Div>
 );
 
-const License = () => (
-  <Div
-    style={{ position: "absolute", right: 40, bottom: 40 }}
-    title="This work is licensed under a Creative Commons Attribution 4.0 International License."
-  >
-    <a
-      rel="license"
-      href="http://creativecommons.org/licenses/by/4.0/"
-      target="_blank"
-    >
-      <img
-        alt="Creative Commons License"
-        style={{ borderWidth: 0 }}
-        src="https://i.creativecommons.org/l/by/4.0/88x31.png"
-      />
-    </a>
-  </Div>
-);
-
 const size = 300;
 
 // TODO
