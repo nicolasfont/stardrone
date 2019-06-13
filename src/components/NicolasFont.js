25d1fa0ae7961f15ae5ec06e00f87943e5387063
Nicolás Font <nicolasfont@gmail.com>
2019-06-12 21:57:30 -0700

    src/components/NicolasFont.js

--- src/components/NicolasFont.js
+++ src/components/NicolasFont.js
@@ -4,10 +4,26 @@ import Div from "./Div";
 import License from "./License";
 import HomeLink from "./HomeLink";
 
+
+
 export default () => (
   <Div centered flex={1} style={{ fontFamily: "Amsterdam", fontSize: 16 }}>
     <HomeLink />
+
+<Div
+    style={{
+      color: "black",
+      fontFamily: "Amsterdam",
+      textDecoration: "none",
+      fontSize: "16px",
+      padding: 0,
+      margin: 0
+    }}
+  >
+
+
     Who the fuck is Nicolás Font
+  </Div>
     <License />
   </Div>
 );
