cc399a76f3b8f9cf4474743926d3fb291c3e4308
Nicolás Font <nicolasfont@gmail.com>
2019-06-12 22:08:47 -0700

    src/components/Tests.js

--- src/components/Tests.js
+++ src/components/Tests.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Div from "./Div";
+import Text from "./Text";
 
 export default () => (
   <Div centered flex={1}>
+    <Text>
     No Tests
+    </Text>
   </Div>
 );
