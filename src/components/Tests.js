4ace78fef8d8628796653c74097b834983598b4b
Nicolás Font <nicolasfont@gmail.com>
2019-06-12 22:17:23 -0700

    src/components/Tests.js

--- src/components/Tests.js
+++ src/components/Tests.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Div from "./Div";
+import HomeLink from "./HomeLink";
 import Text from "./Text";
 
 export default () => (
   <Div centered flex={1}>
+    <HomeLink />
     <Text fontFamily="Impact">No Tests</Text>
   </Div>
 );
