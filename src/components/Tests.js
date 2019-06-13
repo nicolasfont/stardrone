3246eab1cc8cf06a481237d7738872eb4148941f
Nicolás Font <nicolasfont@gmail.com>
2019-06-12 22:26:44 -0700

    src/components/Tests.js

--- src/components/Tests.js
+++ src/components/Tests.js
@@ -3,7 +3,7 @@ import Div from "./Div";
 import HomeLink from "./HomeLink";
 import Text from "./Text";
 
-const { all, map, reverse, splitEvery } = require("ramda");
+const { all, equal, map, reverse, splitEvery } = require("ramda");
 
 const splitIntoGroups = (groupCount, array) =>
   map(reverse, reverse(splitEvery(array.length / groupCount, reverse(array))));
@@ -28,9 +28,10 @@ export default () => (
     <Test it="should split array into given number of groups">
         {() => {
   const groups = splitIntoGroups(3, ["a", "b", "c", "d", "e", "f", "g"]);
-  expect(groups[0]).toEqual(["a", "b", "c"]);
-  expect(groups[1]).toEqual(["d", "e"]);
-  expect(groups[2]).toEqual(["f", "g"]);
+          all(
+            equal(groups[0], ["a", "b", "c"]),
+  equal(groups[1], ["d", "e"]),
+  equal(groups[2], ["f", "g"]))
         }
         }
     </Test>
