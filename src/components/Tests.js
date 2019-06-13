95a7acb4b17ec3f2d194eeb4459ed7e330493f59
Nicolás Font <nicolasfont@gmail.com>
2019-06-12 22:27:17 -0700

    src/components/Tests.js

--- src/components/Tests.js
+++ src/components/Tests.js
@@ -8,13 +8,6 @@ const { all, equal, map, reverse, splitEvery } = require("ramda");
 const splitIntoGroups = (groupCount, array) =>
   map(reverse, reverse(splitEvery(array.length / groupCount, reverse(array))));
 
-it("should split array into given number of groups", () => {
-  const groups = splitIntoGroups(3, ["a", "b", "c", "d", "e", "f", "g"]);
-  expect(groups[0]).toEqual(["a", "b", "c"]);
-  expect(groups[1]).toEqual(["d", "e"]);
-  expect(groups[2]).toEqual(["f", "g"]);
-});
-
 const Test = ({ children, it }) => (
   <Div centered flex={1}>
     A Test
@@ -28,7 +21,7 @@ export default () => (
     <Test it="should split array into given number of groups">
       {() => {
         const groups = splitIntoGroups(3, ["a", "b", "c", "d", "e", "f", "g"]);
-        all(
+        return all(
           equal(groups[0], ["a", "b", "c"]),
           equal(groups[1], ["d", "e"]),
           equal(groups[2], ["f", "g"])
