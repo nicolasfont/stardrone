c345f318198e657365a46de8b5fc65669dc080d5
Nicolás Font <nicolasfont@gmail.com>
2019-07-12 16:33:42 -0700

    src/components/Gomoku.js

--- src/components/Gomoku.js
+++ src/components/Gomoku.js
@@ -2,6 +2,8 @@ import React from "react";
 import HomeLink from "./HomeLink";
 import Div from "./Div";
 
+const Cell = () => <button />;
+
 export default () => (
   <Div centered flex={1}>
     <HomeLink />
