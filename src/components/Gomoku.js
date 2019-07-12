fdab958d48fae98975b3c974c30142a8bb5b81e9
Nicolás Font <nicolasfont@gmail.com>
2019-07-12 16:43:41 -0700

    src/components/Gomoku.js

--- src/components/Gomoku.js
+++ src/components/Gomoku.js
@@ -2,7 +2,7 @@ import React from "react";
 import HomeLink from "./HomeLink";
 import Div from "./Div";
 
-const Cell = value => <button >{value}</button>;
+const Cell = value => <button>{value}</button>;
 
 const Board = ({ values }) => (
   <Div centered flex={1}>
