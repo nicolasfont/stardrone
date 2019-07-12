17b57c562bf7186d6c14825a00ecdb5c40ad7ae1
Nicolás Font <nicolasfont@gmail.com>
2019-07-12 16:24:50 -0700

    src/components/Gomoku.js

--- src/components/Gomoku.js
+++ src/components/Gomoku.js
@@ -2,19 +2,8 @@ import React from "react";
 import HomeLink from "./HomeLink";
 import Div from "./Div";
 
-// responsive
-
 export default () => (
   <Div centered flex={1}>
     <HomeLink />
-    <iframe
-      src="https://www.youtube.com/embed?listType=playlist&modestbranding=1&list=PLVnJFr0ragjfug9bOIVlZHzjEdceWcgws&autoplay=0"
-      title="music"
-      frameBorder={0}
-      width="560"
-      height="315"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    />
-  </Div>
+      </Div>
 );
