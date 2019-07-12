2ef6d43219fc8f7e1634fd18c780110eaae312e4
Nicolás Font <nicolasfont@gmail.com>
2019-07-12 16:27:53 -0700

    src/components/Gomoku.js

--- src/components/Gomoku.js
+++ src/components/Gomoku.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ReactBoard from "react-board";
 import HomeLink from "./HomeLink";
 import Div from "./Div";
 
 export default () => (
   <Div centered flex={1}>
     <HomeLink />
+    <ReactBoard size={2} values={[[0, 1], [2, 3]]} />
   </Div>
 );
