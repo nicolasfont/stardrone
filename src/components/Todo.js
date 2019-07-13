b04f05d3c91a53a20547f72ed36ae10a1aff1ce2
Nicolás Font <nicolasfont@gmail.com>
2019-07-13 11:33:18 -0700

    src/components/Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Div from "./Div";
 import Text from "./Text";
 
 export default () => {
   const [todos, setTodos] = useState(["banans", "pords"]);
   const [input, setInput] = useState("");
+  const onKeyDown = useCallback(e => {
+    if (e.key === "Enter") {
+      setTodos([...todos, input]);
+      setInput("");
+    }
+  });
   return (
     <Div alignItems="center" flex={1} justifyContent="center">
       <Text>Todos</Text>
