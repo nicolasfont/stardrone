7940803fe4f4ce89cc432f0314a277ab85d707bb
Nicolás Font <nicolasfont@gmail.com>
2019-07-13 11:52:37 -0700

    src/components/What.js

--- src/components/What.js
+++ src/components/What.js
@@ -3,7 +3,7 @@ import Div from "./Div";
 import Text from "./Text";
 
 export default () => (
-  <Div flex={1} justifyContent="center">
+  <Div flex={1} textAlign="center">
     <Text>What</Text>
   </Div>
 );
