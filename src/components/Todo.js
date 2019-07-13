bee2e424bc415cb958b11c0a81dde7386f5d854d
Nicolás Font <nicolasfont@gmail.com>
2019-07-13 11:46:30 -0700

    src/components/Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -14,7 +14,7 @@ export default () => {
   });
   return (
     <Div alignItems="center" flex={1} justifyContent="center">
-      <Text>Todo</Text>
+      <Text>Todos</Text>
       <input type="text" onChange={e => setInput(e.target.value)} onKeyDown={onKeyDown} value={input}/>
       {todos.map(todo => <Div key={hash(todo)}>{todo.text}</Div>)}
     </Div>
