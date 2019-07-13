f64e7738792a4e0a66b4776f805cef11e75f2a32
Nicolás Font <nicolasfont@gmail.com>
2019-07-13 02:09:06 -0700

    src/components/Gomoku.js

--- src/components/Gomoku.js
+++ src/components/Gomoku.js
@@ -65,7 +65,7 @@ const Cell = ({ onClick, value, winner }) => (
       justifyContent: "center",
       margin: 0,
       padding: 0,
-      fontWeight: value === winner ? "800" : undefined,
+      fontWeight: value === winner ? "800" : "400",
       borderColor: "black"
     }}
   >
