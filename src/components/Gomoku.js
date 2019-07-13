2eb583b412432b5d54f5d800d72c0206072405fe
Nicolás Font <nicolasfont@gmail.com>
2019-07-12 18:33:34 -0700

    src/components/Gomoku.js

--- src/components/Gomoku.js
+++ src/components/Gomoku.js
@@ -20,7 +20,7 @@ const Cell = ({ value }) => (
 const Board = ({ values }) => (
   <Div flex={1}>
     {values.map((row, i) => (
-      <Div flex={1} key={i} margin={0} padding={0} row>
+      <Div border flex={1} key={i} margin={0} padding={0} row>
         {row.map((value, j) => (
           <Div justifyContent="center" flex={1} key={i + "" + j}>
             <Cell value={value} />
