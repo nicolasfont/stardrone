69d101e256eedc18adcf165cdfb6df1862a02858
Nicolás Font <nicolasfont@gmail.com>
2019-07-13 11:31:46 -0700

    src/components/Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,10 +4,11 @@ import Text from "./Text";
 
 export default () => {
   const [todos, setTodos] = useState(["banans", "pords"]);
+  const [input, setInput] = useState("");
   return (
     <Div alignItems="center" flex={1} justifyContent="center">
       <Text>Todos</Text>
-      <input type="text" onKeyDown={e => console.log(e.key)}/>
+      <input type="text" onChange={e => setInput(e.target.value)} onKeyDown={e => console.log(e.key)} value={input}/>
       {todos.map(todo => <Div key={todo}>{todo}</Div>)}
     </Div>
   );
