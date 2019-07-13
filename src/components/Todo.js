1342a4308354d8b084c2cc4408d9e248b768a74b
Nicolás Font <nicolasfont@gmail.com>
2019-07-13 11:27:07 -0700

    src/components/Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import Div from "./Div";
 import Text from "./Text";
 
 export default () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(["banans", "pords"]);
   return (
     <Div alignItems="center" flex={1} justifyContent="center">
       <Text>Todos</Text>
