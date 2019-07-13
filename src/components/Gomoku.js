d6a6d2f4866183533e04d4a4b5e07849c2909a84
Nicolás Font <nicolasfont@gmail.com>
2019-07-13 00:41:59 -0700

    src/components/Gomoku.js

--- src/components/Gomoku.js
+++ src/components/Gomoku.js
@@ -1,10 +1,12 @@
 import { map, range } from "ramda";
 import React, { useState } from "react";
+import { useToggle } from "../hooks";
 import HomeLink from "./HomeLink";
 import Div from "./Div";
 
-const Cell = ({ value }) => (
+const Cell = ({ onClick, value }) => (
   <button
+    onClick={onClick}
     style={{
       cursor: "pointer",
       display: "flex",
@@ -18,25 +20,27 @@ const Cell = ({ value }) => (
   </button>
 );
 
-const Board = ({ values }) => (
-  <Div flex={1}>
-    {values.map((row, i) => (
-      <Div flex={1} key={i} margin={0} padding={0} row>
-        {row.map((value, j) => (
-          <Div
-            justifyContent="center"
-            flex={1}
-            key={i + "" + j}
-            margin={0}
-            padding={0}
-          >
-            <Cell value={value} />
-          </Div>
-        ))}
-      </Div>
-    ))}
-  </Div>
-);
+const Board = ({ values }) => {
+  return (
+    <Div flex={1}>
+      {values.map((row, i) => (
+        <Div flex={1} key={i} margin={0} padding={0} row>
+          {row.map((value, j) => (
+            <Div
+              justifyContent="center"
+              flex={1}
+              key={i + "" + j}
+              margin={0}
+              padding={0}
+            >
+              <Cell value={value} />
+            </Div>
+          ))}
+        </Div>
+      ))}
+    </Div>
+  );
+};
 
 const emptyMatrix = size =>
   map(() => map(() => " ", range(0, size)), range(0, size));
