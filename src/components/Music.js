380b9d5d3e8c99cc6cd709a9b3e9c8216b87783a
Nicolás Font <nicolasfont@gmail.com>
2019-07-12 17:01:54 -0700

    src/components/Music.js

--- src/components/Music.js
+++ src/components/Music.js
@@ -5,7 +5,7 @@ import Div from "./Div";
 // responsive
 
 export default () => (
-  <Div alignItems="center" textAlign="center" flex={1}>
+  <Div alignItems="center" justifyContent="center" flex={1}>
     <HomeLink />
     <iframe
       src="https://www.youtube.com/embed?listType=playlist&modestbranding=1&list=PLVnJFr0ragjfug9bOIVlZHzjEdceWcgws&autoplay=0"
