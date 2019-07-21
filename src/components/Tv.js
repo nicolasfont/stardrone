13c97f8a52fd18878628150a6c89287d2ceb784e
Nicolás Font <nicolasfont@gmail.com>
2019-07-21 16:27:56 -0700

    src/components/Tv.js

--- src/components/Tv.js
+++ src/components/Tv.js
@@ -8,7 +8,7 @@ export default () => (
   <Div alignItems="center" justifyContent="center" flex={1}>
     <HomeLink />
     <iframe
-      src="https://www.youtube.com/embed?listType=playlist&modestbranding=1&list=PLVnJFr0ragjfug9bOIVlZHzjEdceWcgws&autoplay=0"
+      src="https://www.youtube.com/embed?listType=playlist&modestbranding=1&list=PLVnJFr0ragjc5j_JSMp2ilv6uiPf7AJBU&autoplay=0"
       title="music"
       frameBorder={0}
       width="560"
