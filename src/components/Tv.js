bb3ebf968d8ff13406f5589d963d20714e2dfaf8
Nicolás Font <nicolasfont@gmail.com>
2019-07-21 16:23:04 -0700

    src/components/Tv.js

---
+++ src/components/Tv.js
@@ -0,0 +1,20 @@
+import React from "react";
+import HomeLink from "./HomeLink";
+import Div from "./Div";
+
+// responsive
+
+export default () => (
+  <Div alignItems="center" justifyContent="center" flex={1}>
+    <HomeLink />
+    <iframe
+      src="https://www.youtube.com/embed?listType=playlist&modestbranding=1&list=PLVnJFr0ragjfug9bOIVlZHzjEdceWcgws&autoplay=0"
+      title="music"
+      frameBorder={0}
+      width="560"
+      height="315"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+  </Div>
+);
